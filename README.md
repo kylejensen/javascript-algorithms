22948f69d04aad89154b8ae46c73b69bdc50dbec
Kyle Jensen <kyle.jensen@bynd.com>
2017-01-26 09:06:11 -0500

    Adds bubble sort algorithm

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Simple Sorts
 
 1. [Insertion Sort](insertionsort.js)
-2. [Quick Sort] (quicksort.js)
-3. [Merge Sort] (mergesort.js)
+2. [Quick Sort](quicksort.js)
+3. [Merge Sort](mergesort.js)
+3. [Bubble Sort](bubblesort.js)
---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+/*
+* Bubble Sort Algorithm in JavaScript
+* Average O(n²)
+*/
+
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = (len - 1); i >= 0; i--) {
+        for (var j = (len - 1); j >= 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                var tmp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+
+bubbleSort([22,4,10,6,2,34,8,0,57,4,3,81]); // [ 0, 2, 3, 4, 4, 6, 8, 10, 22, 34, 57, 81 ]
