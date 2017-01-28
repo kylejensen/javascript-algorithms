e2d6c45d258e34225a2de5c029e0b76fe8e219ad
Kyle Jensen <kyle.jensen@bynd.com>
2017-01-28 02:49:57 -0500

    Adds selection sort algorithm

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Simple Sorts
 1. [Insertion Sort](insertionsort.js)
 2. [Quick Sort](quicksort.js)
 3. [Merge Sort](mergesort.js)
-3. [Bubble Sort](bubblesort.js)
+4. [Bubble Sort](bubblesort.js)
+5. [Selection Sort](selectionsort.js)
---
+++ selectionsort.js
@@ -0,0 +1,26 @@
+/*
+* Selection Sort Algorithm in JavaScript
+* Average O(n²)
+*/
+
+function selectionSort(arr) {
+    var len = arr.length;
+    var i, j, min;
+    for (i = 0; i < len; i++) {
+        min = i;
+        for (j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+
+        if (min !== i) {
+            var tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
+}
+
+selectionSort([4,2,6,8,3,9,0,1,2,5,0,123,12,6,32,7,2]); // [ 0, 0, 1, 2, 2, 2, 3, 4, 5, 6, 6, 7, 8, 9, 12, 32, 123 ]
