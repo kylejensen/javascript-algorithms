56e67e12df6d116e8cd991da849406d1bd2a5c5d
Kyle Jensen <kyjen02@gmail.com>
2018-02-17 20:33:31 -0700

    Updates bubble sort and insertion sort files

--- Sorting/bubblesort.js
+++ Sorting/bubblesort.js
@@ -3,18 +3,18 @@
 * Worst-Case O(n²)
 */
 
-function bubbleSort(arr) {
-    var len = arr.length;
-    for (var i = (len - 1); i >= 0; i--) {
-        for (var j = (len - 1); j >= 0; j--) {
-            if (arr[j] < arr[j - 1]) {
-                var tmp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = tmp;
+let bubbleSort = function (arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
             }
         }
     }
     return arr;
-}
+};
 
 bubbleSort([22,4,10,6,2,34,8,0,57,4,3,81]); // [ 0, 2, 3, 4, 4, 6, 8, 10, 22, 34, 57, 81 ]
--- Sorting/insertionsort.js
+++ Sorting/insertionsort.js
@@ -3,10 +3,10 @@
 * Worst-Case O(n²)
 */
 
-function insertionSort (arr) {
-    for (var i = 1; i < arr.length; i++) {
-        var key = arr[i];
-        var j = i - 1;
+let insertionSort = function(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
