e771827adb15e84c61cec7be737c9df29547bb83
Kyle Jensen <kyjen02@gmail.com>
2018-02-17 20:49:58 -0700

    Updates bubble sort file

--- Sorting/bubblesort.js
+++ Sorting/bubblesort.js
@@ -6,7 +6,7 @@
 let bubbleSort = function (arr) {
     let len = arr.length;
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
+        for (let j = 0; j < len - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 let tmp = arr[j];
                 arr[j] = arr[j + 1];
