7ee90d3033f6a00e13a38d5d83be947a8714d551
Kyle Jensen <kyjen02@gmail.com>
2018-02-18 18:46:07 -0700

    Updates selection sort algorithm to es6

--- Sorting/selectionsort.js
+++ Sorting/selectionsort.js
@@ -3,24 +3,22 @@
 * Worst-Case O(n²)
 */
 
-function selectionSort(arr) {
-    var len = arr.length;
-    var i, j, min;
-    for (i = 0; i < len; i++) {
-        min = i;
-        for (j = i + 1; j < len; j++) {
+let selectionSort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
             if (arr[min] > arr[j]) {
                 min = j;
             }
         }
-
         if (min !== i) {
-            var tmp = arr[i];
+            let tmp = arr[i];
             arr[i] = arr[min];
             arr[min] = tmp;
         }
     }
     return arr;
-}
+};
 
 selectionSort([4,2,6,8,3,9,0,1,2,5,0,123,12,6,32,7,2]); // [ 0, 0, 1, 2, 2, 2, 3, 4, 5, 6, 6, 7, 8, 9, 12, 32, 123 ]
