da8eb0e0f09a815bb51b025b62b7563ac99b43ae
Kyle Jensen <kyjen02@gmail.com>
2018-02-19 14:33:45 -0700

    Updates mergesort file

--- Sorting/mergesort.js
+++ Sorting/mergesort.js
@@ -3,12 +3,12 @@
 * Worst-Case O(n log n)
 */
 
-function merge(left, right) {
-    var result = [],
-        leftLen = left.length,
+let merge = (left, right) => {
+    let leftLen = left.length,
         rightLen = right.length,
         l = 0,
-        r = 0;
+        r = 0,
+        result = [];
 
     while (l < leftLen && r < rightLen) {
         if (left[l] < right[r]) {
@@ -17,19 +17,21 @@ function merge(left, right) {
             result.push(right[r++]);
         }
     }
-    return result.concat(left.slice(l)).concat(right.slice(r));
-}
 
-function mergeSort(arr) {
-    var len = arr.length;
+    return result.concat(left.slice(l)).concat(right.slice(r));
+};
+
+let mergeSort = (arr) => {
+    let len = arr.length;
     if (len < 2) {
         return arr;
     }
-    var mid = Math.floor(len / 2),
-        left = arr.slice(0, mid),
-        right = arr.slice(mid);
+
+    let middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
 
     return merge(mergeSort(left), mergeSort(right));
-}
+};
 
 mergeSort([9,3,1,28,5,6,2,3,5,8,4]); // [ 1, 2, 3, 3, 4, 5, 5, 6, 8, 9, 28 ]
