d00603ae20e94ff7b7a7429130f6821cbdfa4ade
Kyle Jensen <kyjen02@gmail.com>
2018-02-20 14:06:30 -0700

    updates binary search file

--- Searching/binarysearch.js
+++ Searching/binarysearch.js
@@ -1,22 +1,42 @@
-function binarySearch(arr, val) {
-    var min = 0,
-        max = arr.length - 1,
+// Iterative version
+
+let binarySearch = (arr, key) => {
+    let max = arr.length - 1,
+        min = 0,
         guess;
 
     while (min <= max) {
-        guess = Math.floor(max + min / 2);
-        if (arr[guess] === val) {
-            return val + " found at position " + guess;
-        } else if (arr[guess] < val) {
+        guess = Math.floor((min + max) / 2);
+
+        if (arr[guess] === key) {
+            return key + " found at position " + guess;
+        } else if (arr[guess] < key) {
             min = guess + 1;
         } else {
             max = guess - 1;
         }
     }
+    return -1;
+};
 
-    return -1; // Error
-}
+// Recursive version
 
-var myArr = [1,2,3,4,5,6,7,8,9,10,13,15,17,19,20,40,56,70];
+let binarySearchRecursive = (arr, key, min, max) => {
+    if (max < min) {
+        return -1;
+    } else {
+        let guess = Math.floor((min + max) / 2);
+        if (arr[guess] === key) {
+            return key + " found at position " + guess;
+        } else if (arr[guess] < key) {
+            return binarySearchRecursive(arr, key, guess + 1, max);
+        } else {
+            return binarySearchRecursive(arr, key, min, guess - 1);
+        }
+    }
+};
 
-binarySearch(myArr, 1); // 1 found at position 0
+let myArr = [1,2,3,4,5,6,7,8,9,10,13,15,17,19,20,40,56,70];
+
+console.log(binarySearch(myArr, 4)); // 4 found at position 3
+console.log(binarySearchRecursive(myArr, 17, 0, myArr.length - 1)); // 17 found at position 12
